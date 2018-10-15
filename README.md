ad5a4634208a55fc884f4e4ec627bec0f2e9dac3
ocramz <zocca.marco@gmail.com>
2018-10-15 21:25:41 +0200

    first

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # core
-DataHaskell monorepo
+
+[![Build Status](https://travis-ci.org/DataHaskell/core.png)](https://travis-ci.org/DataHaskell/core)
+
+TODO Description.
---
+++ src/Lib.hs
@@ -0,0 +1,11 @@
+-- | A library to do stuff.
+module Lib
+    (
+      ourAdd
+    ) where
+
+-- | Add two 'Int' values.
+ourAdd :: Int  -- ^ left
+       -> Int  -- ^ right
+       -> Int  -- ^ sum
+ourAdd x y = x + y
