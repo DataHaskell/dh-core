bac21d0fa69d150e0b27c4f2c08ca11119c77461
ocramz <zocca.marco@gmail.com>
2018-10-17 22:48:04 +0200

    add Core.Numeric.LinearAlgebra.Class #3

---
+++ dh-core/src/Core/Numeric/LinearAlgebra/Class.hs
@@ -0,0 +1,15 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Core.Numeric.LinearAlgebra.Class
+-- Copyright   :  (c) Marco Zocca 2017-2018
+-- License     :  see the file LICENSE
+--
+-- Maintainer  :  ocramz fripost org
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- Typeclasses for linear algebra and related concepts
+--
+-----------------------------------------------------------------------------
+module Core.Numeric.LinearAlgebra.Class where
+
