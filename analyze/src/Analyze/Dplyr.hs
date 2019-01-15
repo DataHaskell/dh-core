43734de038621e2861928b99678d5a2008154f55
ocramz <zocca.marco@gmail.com>
2019-01-15 02:48:53 +0100

    add Dplyr.looks

--- analyze/src/Analyze/Dplyr.hs
+++ analyze/src/Analyze/Dplyr.hs
@@ -4,6 +4,7 @@ module Analyze.Dplyr where
 import Analyze.RFrame
 import Analyze.Common
 
+import qualified Data.Foldable as F
 import qualified Data.Vector as V
 import qualified Data.HashMap.Strict as HM
 
@@ -24,9 +25,44 @@ filterByKey qv k (RFrame ks hm vs) = do
     ff vrow = do
       i <- HM.lookup k hm
       v <- vrow V.!? i
-      pure $ qv v 
+      pure $ qv v
+
+
   
 
+innerJoin q row1 fr2 = undefined
+  where
+
+-- | Right half of the loop for a hash-join    
+looks :: (Foldable t, Key k, Eq v) =>
+         v
+      -> k
+      -> HM.HashMap k Int
+      -> t (V.Vector v)
+      -> Maybe (HM.HashMap k v, Maybe v)
+looks v1 k2 hm2 vecs2 = F.foldlM insf z vecs2 
+  where
+    z = (HM.empty, Nothing)
+    insf (acc, _) vec2 = do
+      v2 <- look k2 hm2 vec2
+      if v1 == v2
+        then
+          pure (acc, Just v2)
+        else do
+          let acc' = HM.insert k2 v2 acc
+          pure (acc', Nothing)
+      
+    
+look :: Key k =>
+        k
+     -> HM.HashMap k Int
+     -> V.Vector b
+     -> Maybe b
+look k hm vrow = do
+  i <- HM.lookup k hm
+  vrow V.!? i
+
+    
 
 
 -- -- Playground
