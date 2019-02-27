09d300f83d5e6ce9d8e1a380bfa6cb9551a3be39
Ghosal <ghosalatreyee@gmail.com>
2019-02-28 01:10:04 +0530

    attempting an ad-hoc fix to the too-many-files-changed issue

--- analyze/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-	0.2.0
-	Integrated within DataHaskell/dh-core
-	Adapted to latest `tasty-hspec` API
-	Builds with GHC 8.2.2 and 8.4.3
--- datasets/bench/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import qualified Numeric.Dataloader.Benchmark
-
-main :: IO ()
-main = Numeric.Dataloader.Benchmark.main
-
