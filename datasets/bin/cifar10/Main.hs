02af0b351e0ee12b35df32741ad2363c863a652a
Marco Zocca <marco.zocca@unfoldml.com>
2022-02-01 17:06:50 +0100

    datasets/cifar10-provision : show a nice progress bar

--- datasets/bin/cifar10/Main.hs
+++ datasets/bin/cifar10/Main.hs
@@ -6,6 +6,7 @@ import Numeric.Datasets.CIFAR10 (CIFARImage(..), cifar10)
 import System.FilePath
 import System.Directory (XdgDirectory(..), getXdgDirectory, createDirectoryIfMissing, listDirectory)
 import qualified Streaming.Prelude as S
+import System.ProgressBar (newProgressBar, defStyle, Progress(..), incProgress)
 
 main :: IO ()
 main = provision
@@ -16,16 +17,21 @@ provision = do
   xdgCache <- getXdgDirectory XdgCache "datasets-hs"
   let imfolder = xdgCache </> "cifar-10-imagefolder"
   createDirectoryIfMissing True imfolder
+  -- progress bar
+  let n = 60000
+  pb <- newProgressBar defStyle 10 (Progress 0 n ())
+  let
+    go :: FilePath -> CIFARImage -> IO ()
+    go cachefolder (CIFARImage (im, lbl)) = do
+      let labelfolder = cachefolder </> show lbl
+      createDirectoryIfMissing True labelfolder
+      ix <- length <$> listDirectory labelfolder
+      let
+        fname = show lbl ++ "_" ++ show ix ++ ".png"
+      writePng (labelfolder </> fname) im
+      incProgress pb 1 -- increment progress bar
 
   -- build the image folder
   S.mapM_ (go imfolder) $ streamDataset cifar10
- where
-  go :: FilePath -> CIFARImage -> IO ()
-  go cachefolder (CIFARImage (im, lbl)) = do
-    let labelfolder = cachefolder </> show lbl
-    createDirectoryIfMissing True labelfolder
-    ix <- length <$> listDirectory labelfolder
-    let
-      fname = show lbl ++ "_" ++ show ix ++ ".png"
-    writePng (labelfolder </> fname) im
-    putStrLn $ show fname
+
+
